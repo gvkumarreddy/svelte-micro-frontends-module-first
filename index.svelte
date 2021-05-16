fc9269c83da44f059db6633be26f048f4ac6ff2a
Vijay <gvkumar.reddy@gmail.com>
2021-05-16 23:36:57 +0800

    MOdule added

---
+++ index.svelte
@@ -0,0 +1,18 @@
+<script>
+   
+   import DraggableModal from '../../lib/DraggableModal/index.svelte';
+
+    let showModal = false;
+
+    const toggleModal = () => {
+        showModal = !showModal;
+    }
+</script>
+
+<div>Another content</div>
+<button style="width: 150px" on:click={toggleModal} >Show Modal</button>
+{#if showModal}
+<DraggableModal left={200} top={200} title="Drag Me">
+    Draggable Modal Content
+</DraggableModal>
+{/if}
